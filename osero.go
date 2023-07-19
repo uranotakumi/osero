514e4896431b43205b877a92c08647c962f2e7e5
Zihan Jin <y.kodachi.study@gmail.com>
2023-07-19 00:24:56 +0000

    osero

--- osero.go
+++ osero.go
@@ -11,7 +11,7 @@ func print_board(a [8][8]string) {
 	}
 }
 
-func input_bord() [2]int {
+func input_board() [2]int {
 	var n [2]int
 	fmt.Scan(&n[0], &n[1])
 	return n
@@ -31,8 +31,91 @@ func check_number(a [8][8]string) {
 	fmt.Println("O:%v, X:%v.", n[0], n[1])
 }
 
+func check_change(a [8][8]string, n [2]int, c int) [8][8]string {
+	if c%2 == 0 {
+		// yoko
+		for i := 0; i < 8; i++ {
+			if i < n[1]-1 {
+				if a[n[0]-1][i] == "O" {
+					for j := i + 1; j <= n[1]-2; j++ {
+						a[n[0]-1][j] = "O"
+					}
+				}
+			}
+			if i > n[1]-1 {
+				if a[n[0]-1][i] == "O" {
+					for j := n[1] - 2; j <= i-1; j++ {
+						a[n[0]-1][j] = "O"
+					}
+				}
+			}
+		}
+		// tate
+		for i := 0; i < 8; i++ {
+			if i < n[0]-1 {
+				if a[i][n[1]-1] == "O" {
+					for j := i + 1; j <= n[0]-2; j++ {
+						a[j][n[1]-1] = "O"
+					}
+				}
+			}
+			if i > n[0]-1 {
+				if a[i][n[1]-1] == "O" {
+					for j := n[0] - 2; j <= i-1; j++ {
+						a[j][n[1]-1] = "O"
+					}
+				}
+			}
+		}
+	} else {
+		// yoko
+		for i := 0; i < 8; i++ {
+			if i < n[1]-1 {
+				if a[n[0]-1][i] == "X" {
+					for j := i + 1; j <= n[1]-2; j++ {
+						a[n[0]-1][j] = "X"
+					}
+				}
+			}
+			// koko ga hen
+			/*
+				if i > n[1]-1 {
+					if a[n[0]-1][i] == "X" {
+						for j := n[1] - 2; j <= i-1; j++ {
+							a[n[0]-1][j] = "X"
+						}
+					}
+				}
+			*/
+		}
+		// tate
+		for i := 0; i < 8; i++ {
+			if i < n[0]-1 {
+				if a[i][n[1]-1] == "X" {
+					for j := i + 1; j <= n[0]-2; j++ {
+						a[j][n[1]-1] = "X"
+					}
+				}
+			}
+			// kokoga hen
+			/*
+				if i > n[0]-1 {
+					if a[i][n[1]-1] == "X" {
+						for j := n[0] - 2; j <= i-1; j++ {
+							a[j][n[1]-1] = "X"
+						}
+					}
+				}
+			*/
+		}
+	}
+
+	return a
+}
+
 func main() {
-	a := [8][8]string{{".", ".", ".", ".", ".", ".", ".", "."},
+	a := [8][8]string{
+		{".", ".", ".", ".", ".", ".", ".", "."},
 		{".", ".", ".", ".", ".", ".", ".", "."},
 		{".", ".", ".", ".", ".", ".", ".", "."},
 		{".", ".", ".", "O", "X", ".", ".", "."},
@@ -45,7 +128,7 @@ func main() {
 	for i := 0; i < 60; i++ {
 		fmt.Printf("Player %v 's turn. Enter position (x,y)\n", i%2+1)
 		var n [2]int
-		n = input_bord()
+		n = input_board()
 		if (a[n[0]-1][n[1]-1] == "O") || (a[n[0]-1][n[1]-1] == "X") {
 			fmt.Println("Cheat!!!!!")
 			break
@@ -55,6 +138,7 @@ func main() {
 
 			// win := check_winner(a)
 
+			a = check_change(a, n, i)
 			print_board(a)
 			check_number(a)
 			/*
@@ -68,6 +152,7 @@ func main() {
 
 			// win := check_winner(a)
 
+			a = check_change(a, n, i)
 			print_board(a)
 			check_number(a)
 			/*
@@ -77,6 +162,6 @@ func main() {
 				}
 			*/
 		}
-		fmt.Println("Draw.")
 	}
+	fmt.Println("Draw.")
 }
